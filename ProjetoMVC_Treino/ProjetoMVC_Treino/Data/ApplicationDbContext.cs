19b347df874a192857104090b0743dab5f5125ce
Guilherme Paixão dos Santos <paixaoguilherme79@gmail.com>
2024-01-13 03:12:03 -0300

    Adicionando a Conexão com o Banco de Dados

---
+++ ProjetoMVC_Treino/ProjetoMVC_Treino/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoMVC_Treino.Models;
+
+namespace ProjetoMVC_Treino.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<EmprestimosModel> Emprestimos { get; set; }
+    }
+}
