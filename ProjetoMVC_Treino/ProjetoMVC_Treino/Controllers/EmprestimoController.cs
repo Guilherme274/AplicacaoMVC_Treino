43b0b6fb58885df76864e112c8cc4d686431bee1
Guilherme Paixão dos Santos <paixaoguilherme79@gmail.com>
2024-01-14 22:52:43 -0300

    Adicionando uma Tabela com as informações do Empréstimo dos Livros
    Através do SGBD eu adicionei alguns dados dentro da tabela. Na aplicação MVC tornei possível que estes dados fossem vistos dentro do HTML, fazendo meu controlador conhecer o contexto do Banco De Dados e o enumerador que foi criado para armazenar a quantidade de Empréstimos

--- ProjetoMVC_Treino/ProjetoMVC_Treino/Controllers/EmprestimoController.cs
+++ ProjetoMVC_Treino/ProjetoMVC_Treino/Controllers/EmprestimoController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC_Treino.Data;
+using ProjetoMVC_Treino.Models;
+using System.Collections.Generic;
 
 namespace ProjetoMVC_Treino.Controllers
 {
     public class EmprestimoController : Controller
     {
+
+        readonly private ApplicationDbContext _db;
+
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
+            return View(emprestimos);
         }
     }
 }
--- ProjetoMVC_Treino/ProjetoMVC_Treino/obj/Debug/net8.0/ProjetoMVC_Treino.AssemblyInfo.cs
+++ ProjetoMVC_Treino/ProjetoMVC_Treino/obj/Debug/net8.0/ProjetoMVC_Treino.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoMVC_Treino")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19b347df874a192857104090b0743dab5f5125ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c65ff2ea7ce3dc870ce667f237d15f9309f825d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoMVC_Treino")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoMVC_Treino")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
